629b252031b3a43dc61a1946806339ca309c5550
Mamatha3434 <MamathaReddygari@my.unt.edu>
2024-03-17 14:43:12 -0500

    Implemented Admin Page to manage Employee Profiles

--- src/admin_page/AdminPage.js
+++ src/admin_page/AdminPage.js
@@ -3,50 +3,185 @@ import { DataGrid } from '@mui/x-data-grid';
 import adminDatajson from './../data_folders/adminData.json'
 import { useEffect, useState } from 'react';
 import admstyles from './css-folder/AdminPage.module.css'
-export default function AdminPage(){
+import OfPerson from './OfAPerson';
+import EditDetailsOfPerson from './EditDetailsOfPerson';
+export default function AdminPage() {
 
-    let [empData,setEmpData]=useState([]);
+    let [empData, setEmpData] = useState([]);
+    let [editRowData, setEditRowData] = useState([]);
+    let [AttendanceData, setAttendanceData] = useState([]);
+
+    let [viewStatsData, setViewStatsData] = useState([]);
+
+
+
+
+
+
+
+    let [showAdminPage, setShowAdminPage] = useState(true);
+    let [showPersonPage, setShowPersonPage] = useState(false);
+    let [showEditScreen, setShowEditScreen] = useState(false);
+
     // let [earnings]
     let columnHeaders = [
-        { field: 'name', headerName: 'Username', flex: 1, align: 'center', headerAlign: 'center' }, 
-        { field: 'date', headerName: 'Username', flex: 1, align: 'center', headerAlign: 'center' }, 
+        { field: 'id', headerName: 's_no', flex: 1, align: 'center', headerAlign: 'center' },
 
-        { field: 'activity_id', headerName: 'UserId', flex: 1, align: 'center', headerAlign: 'center' }, 
-        { field: 'hours', headerName: 'Hours worked', flex: 1, align: 'center', headerAlign: 'center' },
+        { field: 'employee_id', headerName: 'EMPOLOYEE ID', flex: 1, align: 'center', headerAlign: 'center' },
+        { field: 'user_id', headerName: 'USER ID', flex: 1, align: 'center', headerAlign: 'center' },
+
+        { field: 'total_hours_worked', headerName: 'Total Hours', flex: 1, align: 'center', headerAlign: 'center' },
 
         { field: 'earnings', headerName: 'earnings', flex: 1, align: 'center', headerAlign: 'center' },
-        { field: 'ofPerson',headerName: 'person' ,flex:1,align:'center',headerAlign:'center',renderCell:(params)=>{ }},
+        {
+            field: "action", headerName: "Action", sortable: false, width: 225, fontSize: 12, fontWeight: 'bold',
+            renderCell: (params) => {
+              return (
+                <div className={admstyles.viewButton}>
+                  <a  onClick={() => { personStatsFunction(params.row) }}> VIEW STATS</a>
+                 
+                </div>
+              )
+            }
+          },
+          {
+            field: "Edit", headerName: "EDIT PAYROLL", sortable: false, width: 225, fontSize: 12, fontWeight: 'bold',
+            renderCell: (params) => {
+              return (
+                <div className={admstyles.viewButton}>
+                  <a  onClick={() => { EditDetailsScreenFunction(params.row) }}> EDIT PAYROLL</a>
+                 
+                </div>
+              )
+            }
+          },
+
 
     ]
 
-    useEffect(()=>{
+    //     useEffect(()=>{
 
-        let adminData=adminDatajson.map((e)=>{
-            return {
-                ...e,
-                earnings: '$'+e.hours*10
+    //         let adminData=adminDatajson.map((e)=>{
+    //             return {
+    //                 ...e,
+    //                 earnings: '$'+e.hours*10
+    //             }
+    //         })
+
+
+    //    setEmpData(adminData);
+
+    //     },[adminDatajson])
+
+    let backtoAdminpage=()=>{
+        setShowPersonPage(false);
+        setShowEditScreen(false);
+        setShowAdminPage(true);
+    }
+
+     let personStatsFunction=(params)=>{
+
+      setEditRowData(params);
+
+        console.log("##params",params);
+        setShowPersonPage(true);
+        setShowAdminPage(false);
+
+     }
+
+     let EditDetailsScreenFunction=(params)=>{
+
+        console.log("##params",params);
+
+        setEditRowData(params);
+        setShowEditScreen(true);
+        setShowAdminPage(false);
+     }
+
+    let fetchAttendanceDetails = async () => {
+        //http://localhost:8081/ttp-application/getEmployeeAttendanceDetails
+        let response = await fetch('http://localhost:8081/ttp-application/getEmployeeAttendanceDetails');
+        let responseJson = await response.json();
+
+        console.log("##attendance response", responseJson);
+       setAttendanceData(responseJson);
+
+           
+        let index = 1;
+        const groupedData = responseJson.reduce((groups, entry) => {
+            const { user_id, employee_id, hours_worked,pay_scale } = entry;
+            if (!groups[user_id]) {
+                groups[user_id] = { 
+                    user_id: user_id, 
+                    employee_id: employee_id, 
+                    total_hours_worked: 0,
+                     id: index ,
+                    earnings:0,
+                };
+                index++;
             }
-        })
+            groups[user_id].total_hours_worked += hours_worked;
+            groups[user_id].earnings += (hours_worked*pay_scale);
+
+            return groups;
+        }, {});
+
+        // Convert grouped data object to an array of objects
+
+        console.log("##groupedData", groupedData)
+        const result = Object.values(groupedData);
+        console.log("##result", result);
+        setEmpData(result);
+    }
+
+    let fetchPersonDetails = async () => {
+
+        let response = await fetch('http://localhost:8081/ttp-application/getEmployeeDetails');
+        let responseJson = await response.json();
+        console.log("##person response", responseJson);
+
+    }
 
 
-   setEmpData(adminData);
+    useEffect(() => {
+        fetchAttendanceDetails();
 
-    },[adminDatajson])
+        fetchPersonDetails();
+    }, [])
 
-    return(
+    return (
         <div className={admstyles.body}>
-           <div className={admstyles.title}>ADMIN PAGE</div>
-            <div className={admstyles.tableContainer}>
+            <div className={admstyles.title}>ADMIN PAGE</div>
+            { showAdminPage &&
+                <div className={admstyles.tableContainer}>
                 <DataGrid
 
                     autoHeight
                     columns={columnHeaders}
                     rows={empData}
-                                          
+
                 >
 
                 </DataGrid>
             </div>
+            }
+
+            {
+                showPersonPage &&
+                <div>
+                    <OfPerson  attendanceData={AttendanceData} data={editRowData} close={backtoAdminpage}/> 
+
+                </div>
+
+           }
+
+           {
+            showEditScreen &&
+            <div>
+                <EditDetailsOfPerson  data={editRowData} close={backtoAdminpage}/>
+                </div>
+           }
+            
 
         </div>
     )
