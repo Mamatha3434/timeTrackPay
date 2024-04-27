f3e22055ac12af0d604114d5e0d87b47691c4249
yasasri <yasasrivaddi2000@gmail.com>
2024-04-26 22:47:40 -0500

    Implemented fetching standard payrolls and setting max pay scale state

--- src/admin_page/EditDetailsOfPerson.js
+++ src/admin_page/EditDetailsOfPerson.js
@@ -1,30 +1,53 @@
 
 import { useEffect, useState } from 'react';
 import admstyles from './css-folder/AdminPage.module.css'
+import { Alert } from '@mui/material';
 
 
 
 
 const EditDetailsOfPerson = (params) => {
 
+    let [showalert, setShowAlert] = useState(false);
+
     let { close, data } = params;
     let [empObj, setEmpObj] = useState({
         employeeId: '',
         userId: '',
         payscale: '',
-        newpayscale: ''
+        newpayscale: '',
+        currentRoleName:''
     })
 
 
+    let [maxPayscale, setMaxPayscale] = useState(0);
+
+      let fetchStadardPayrolls = async (current_role_name) => {
+
+        let standardPayRollResponse = await fetch('http://localhost:8081/ttp-application/standardPayRolls');
+        let standardPayRollResponseJson = await standardPayRollResponse.json();
+        console.log('standardPayRollResponseJson', standardPayRollResponseJson);
+
+        let maxPayscale = standardPayRollResponseJson.filter((e) => { return e.role_name == current_role_name })[0].standard_pay_scale;
+
+        setMaxPayscale(maxPayscale);
+          
+
+      }
 
     useEffect(() => {
         console.log('data', data);
+
+
+        fetchStadardPayrolls(data.current_role_name);
+
 
 
         setEmpObj((prev) => {
             let newObj = { ...prev };
             newObj.employeeId = data.employee_id;
             newObj.userId = data.user_id;
+            newObj.currentRoleName=data.current_role_name;
             return newObj;
         })
 
@@ -58,20 +81,46 @@ const EditDetailsOfPerson = (params) => {
     }
 
 
+
+
      let saveEmployeeDetails =async()=>{
         let payload = {
             newpayscale: empObj.newpayscale,
             employeeId: empObj.employeeId
         }
-        const response = await fetch('http://localhost:8081/ttp-application/updatePersonPayRoll', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(payload)
-        });
 
-        close();
+        if(empObj.newpayscale>maxPayscale)
+            {
+                console.log('Payroll cannot be greater than maximum payroll');
+                setTimeout(() => {
+
+                    setShowAlert(false);
+                    
+                }, 1000);
+
+                setShowAlert(true);
+
+                // return;
+
+            }
+            else
+            {
+                const response = await fetch('http://localhost:8081/ttp-application/updatePersonPayRoll', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(payload)
+                });
+        
+                console.log(response);
+                close();
+            }
+
+       
+
+       
+        // close();
 
      }
 
@@ -80,6 +129,13 @@ const EditDetailsOfPerson = (params) => {
             <div className={admstyles.closeButton}>
                 <button onClick={close}>Back</button>
             </div>
+            <div style={admstyles.alertBox}>
+            {
+                showalert && <Alert severity="error">Payroll cannot be greater than maximum payroll</Alert>
+            }
+            </div>
+
+            
 
 
             <div className={admstyles.box_container}>
@@ -97,6 +153,18 @@ const EditDetailsOfPerson = (params) => {
                         {empObj.payscale}
                     </span>
                 </div>
+                <div className={admstyles.boxy}>
+                    <label>CURRENT ROLE NAME:</label>
+                    <span>
+                        {empObj.currentRoleName}
+                    </span>
+                </div>
+                <div className={admstyles.boxy}>
+                    <label>MAXIMUM PAY ROLL:</label>
+                    <span>
+                        {maxPayscale}
+                    </span>
+                </div>
 
                 <div className={admstyles.boxy}>
                     <label>EDIT PAYROLL:</label>
@@ -111,6 +179,8 @@ const EditDetailsOfPerson = (params) => {
             <div className={admstyles.saveButton}>
                 <button onClick={saveEmployeeDetails}>save</button>
             </div>
+
+            
 
         </div>
     )
